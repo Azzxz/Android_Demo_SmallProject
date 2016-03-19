b9e87ee1679a07d0d95c1f2674449268065c7981
IamXiaRui <iamxiarui@foxmail.com>
2016-03-19 14:30:09 +0800

    XML文件的生成与解析

--- Android_XMLDemo/src/xr/android_xmldemo/util/SmsUtil.java
+++ Android_XMLDemo/src/xr/android_xmldemo/util/SmsUtil.java
@@ -1,5 +1,9 @@
 package xr.android_xmldemo.util;
 
+/*
+ * 生成XML文件的两种方式
+ * 
+ * */
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -14,6 +18,7 @@ import xr.android_xmldemo.dao.SmsDao;
 
 public class SmsUtil {
 
+	// 生成XML文件的第一种方式 ： 直接写文件
 	public static boolean SaveSms(Context context) {
 
 		ArrayList<Sms> al = SmsDao.getSms();
@@ -67,24 +72,37 @@ public class SmsUtil {
 		return false;
 	}
 
+	// 生成XML文件的第二种方式 ： 利用XMLSerializer类来生成XML文件
+
 	public static boolean SaveSmsTwo(Context context) {
 
 		ArrayList<Sms> al = SmsDao.getSms();
 
+		// 第一步 生成一个XML序列化对象
 		XmlSerializer xs = Xml.newSerializer();
 
 		try {
+			// 第一个参数 ： 存入文件的名称 第二个参数 文件的模式 私有模式 第三个 文件的编码格式
 			xs.setOutput(context.openFileOutput("SmsSaveXML.xml", Context.MODE_PRIVATE), "utf-8");
+
+			// XML文件的声明头 编码格式 是否独立
 			xs.startDocument("utf-8", true);
 
+			// XML文件的开始节点 根节点 第一个参数 命名空间 第二个参数 节点名称
 			xs.startTag(null, "Smss");
 
+			// 循环插入节点及其内容
 			for (Sms sms : al) {
+				// 开始节点
 				xs.startTag(null, "Sms");
+
+				// 开始节点中的属性
 
 				xs.attribute(null, "id", sms.id + "");
 
+				// 节点内的节点
 				xs.startTag(null, "num");
+				// 节点的属性
 				xs.text(sms.num);
 				xs.endTag(null, "num");
 
@@ -96,11 +114,14 @@ public class SmsUtil {
 				xs.text(sms.date);
 				xs.endTag(null, "date");
 
+				// 有开始 就有结束节点
 				xs.endTag(null, "Sms");
 			}
 
+			// 有开始 一定有结束 结束节点
 			xs.endTag(null, "Smss");
 
+			// 结束文件 完成XML的序列化
 			xs.endDocument();
 
 			return true;
