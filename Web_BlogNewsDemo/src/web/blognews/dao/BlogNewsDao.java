10ae2b1d31ed80d594474edb75217a062124a503
IamXiaRui <iamxiarui@foxmail.com>
2016-03-27 23:08:41 +0800

    网页博客服务端

--- Web_BlogNewsDemo/src/web/blognews/dao/BlogNewsDao.java
+++ Web_BlogNewsDemo/src/web/blognews/dao/BlogNewsDao.java
@@ -8,56 +8,62 @@ import java.util.ArrayList;
 
 import web.blognews.bean.BlogBean;
 
+/**
+ * @ClassName: BlogNewsDao
+ * @Description: 连接数据库 得到数据
+ * @author iamxiarui@foxmail.com
+ * @date 2016年3月27日 下午11:08:01
+ * 
+ */
 public class BlogNewsDao {
 
-	public static ArrayList<BlogBean> getBlogs(){
+	public static ArrayList<BlogBean> getBlogs() {
 		Connection connection = null;
-        Statement stmt = null;
-        ResultSet rs = null;
-        String sql = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		String sql = null;
 
-        try {
-            /*****填写数据库相关信息(请查找数据库详情页)*****/
-            String databaseName = "blog_db";
-            String host = "localhost";
-            String port = "3306";
-            String username = "root"; //用户AK
-            String password = "XF802369"; //用户SK
-            String driverName = "com.mysql.jdbc.Driver";
-            String dbUrl = "jdbc:mysql://";
-            String serverName = host + ":" + port + "/";
-            String connName = dbUrl + serverName + databaseName;
+		try {
+			/***** 填写数据库相关信息(请查找数据库详情页) *****/
+			String databaseName = "blog_db";
+			String host = "localhost";
+			String port = "3306";
+			String username = "root"; // 用户AK
+			String password = "XF802369"; // 用户SK
+			String driverName = "com.mysql.jdbc.Driver";
+			String dbUrl = "jdbc:mysql://";
+			String serverName = host + ":" + port + "/";
+			String connName = dbUrl + serverName + databaseName;
 
-            /******接着连接并选择数据库名为databaseName的服务器******/
-            Class.forName(driverName);
-            connection = DriverManager.getConnection(connName, username,
-                    password);
-            stmt = connection.createStatement();
-            /******至此连接已完全建立，就可对当前数据库进行相应的操作了*****/
-            /******接下来就可以使用其它标准mysql函数操作进行数据库操作*****/
-            //创建一个数据库表
-            sql = "select * from blogs order by id desc";
-            ResultSet rss = stmt.executeQuery(sql);
-            ArrayList<BlogBean> arrayList = new ArrayList<BlogBean>();
-            if(rss != null){
-            	while(rss.next()){
-            		int id = rss.getInt("id");
-            		String title = rss.getString("title");
-            		String des = rss.getString("des");
-            		String url = rss.getString("url");
-            		
-            		BlogBean blog = new BlogBean();
-            		blog.setId(id);
-            		blog.setTitle(title);
-            		blog.setDes(des);
-            		blog.setUrl(url);
-            		arrayList.add(blog);            		
-            	}
-            	return arrayList;          	
-            }       
-        } catch (Exception e) {
-        	e.printStackTrace();
-        }
-        return null;	
+			/****** 接着连接并选择数据库名为databaseName的服务器 ******/
+			Class.forName(driverName);
+			connection = DriverManager.getConnection(connName, username, password);
+			stmt = connection.createStatement();
+			/****** 至此连接已完全建立，就可对当前数据库进行相应的操作了 *****/
+			/****** 接下来就可以使用其它标准mysql函数操作进行数据库操作 *****/
+			// 创建一个数据库表
+			sql = "select * from blogs order by id desc";
+			ResultSet rss = stmt.executeQuery(sql);
+			ArrayList<BlogBean> arrayList = new ArrayList<BlogBean>();
+			if (rss != null) {
+				while (rss.next()) {
+					int id = rss.getInt("id");
+					String title = rss.getString("title");
+					String des = rss.getString("des");
+					String url = rss.getString("url");
+
+					BlogBean blog = new BlogBean();
+					blog.setId(id);
+					blog.setTitle(title);
+					blog.setDes(des);
+					blog.setUrl(url);
+					arrayList.add(blog);
+				}
+				return arrayList;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
--- Web_BlogNewsDemo/src/web/blognews/servlet/BlogNewsServlet.java
+++ Web_BlogNewsDemo/src/web/blognews/servlet/BlogNewsServlet.java
@@ -17,24 +17,28 @@ import web.blognews.bean.BlogBean;
 import web.blognews.dao.BlogNewsDao;
 
 /**
- * Servlet implementation class Servlet
+ * @ClassName: BlogNewsServlet
+ * @Description: 网页博客服务端
+ * @author iamxiarui@foxmail.com
+ * @date 2016年3月27日 下午11:06:58
+ * 
  */
 @WebServlet("/Servlet")
 public class BlogNewsServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public BlogNewsServlet() {
-        super();
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public BlogNewsServlet() {
+		super();
+	}
 
 	/**
 	 * @see BlogNewsServlet#init(ServletConfig)
 	 */
 	public void init(ServletConfig config) throws ServletException {
-		
+
 	}
 
 	/**
@@ -44,15 +48,24 @@ public class BlogNewsServlet extends HttpServlet {
 		super.destroy();
 	}
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	/*
+	 * Title: doGet Description:得到数据库中的数据 并封装到JSON中
+	 * 
+	 * @param request 请求
+	 * 
+	 * @param response 恢复
+	 * 
+	 * @throws ServletException
+	 * 
+	 * @throws IOException
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		try{
-			//从数据库获取新闻数据
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			// 从数据库获取新闻数据
 			ArrayList<BlogBean> blogs = BlogNewsDao.getBlogs();
 
-			//将list中的数据封装成一个jsonArray对象
+			// 将list中的数据封装成一个jsonArray对象
 			JSONArray jsonArray = new JSONArray();
 			for (BlogBean blogBean : blogs) {
 				JSONObject newsJson = new JSONObject();
@@ -60,29 +73,31 @@ public class BlogNewsServlet extends HttpServlet {
 				newsJson.put("title", blogBean.getTitle());
 				newsJson.put("des", blogBean.getDes());
 				newsJson.put("url", blogBean.getUrl());
-				jsonArray.put(newsJson); //把3个{} json对象添加到jsonArray里
+				jsonArray.put(newsJson); // 把3个{} json对象添加到jsonArray里
 
 			}
 
-			//将jsonArray对象作为一个json对象，用来返回给客户端
+			// 将jsonArray对象作为一个json对象，用来返回给客户端
 			JSONObject allBlogsJson = new JSONObject();
-			allBlogsJson.put("blogs", jsonArray);  //"blogs"--作为外部json对象名(键)， jsonArray---作为外部json对象值内容(值) 
+			allBlogsJson.put("blogs", jsonArray); // "blogs"--作为外部json对象名(键)，
+													// jsonArray---作为外部json对象值内容(值)
 
-			//外部json对象初始化新闻数据赋值完毕，转成字符串，响应给客户端
-			response.getOutputStream().write(allBlogsJson.toString().getBytes("utf-8")); 
+			// 外部json对象初始化新闻数据赋值完毕，转成字符串，响应给客户端
+			response.getOutputStream().write(allBlogsJson.toString().getBytes("utf-8"));
 
-		}catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
-	
 }
